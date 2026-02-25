0dc7e5992cfa23a9411aa1b21439d0589f79a191
parkhamin <parkhamint@naver.com>
2026-02-25 15:11:03 +0900

    불린형

---
+++ 불린형/index.js
@@ -0,0 +1,26 @@
+// 불린
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(3 >= 2);
+console.log(3 <= 3);
+
+// 두 값이 자료형과 값이 같아? -> true
+console.log(3 === 3);
+
+// 두 값이 자료형과 값이 달라? -> false
+console.log(3 !== 3);
+
+console.log('Hamin' === 'Hamin');
+console.log('Hamin' !== 'hamin');
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(!true);

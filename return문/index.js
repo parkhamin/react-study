8778a7e33c43fe3b2b0d39545803b05b096ed783
parkhamin <parkhamint@naver.com>
2026-02-25 14:34:15 +0900

    return문

---
+++ return문/index.js
@@ -0,0 +1,14 @@
+function getTwo() {
+  return 2;
+}
+
+console.log(getTwo());
+
+function getTwice(number) {
+  return number * 2;
+}
+
+let x = getTwice(5);
+let y = getTwice(2);
+
+console.log(x * y);

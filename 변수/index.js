0efb5672afe37d9455d3caac50ebf9231f2e5dd3
parkhamin <parkhamint@naver.com>
2026-02-25 13:54:22 +0900

    first commit

---
+++ 변수/index.js
@@ -0,0 +1,9 @@
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+
+console.log(espressoPrice * 2);
+console.log(lattePrice * 4 + mochaPrice * 2);
+console.log(espressoPrice + lattePrice * 4 + mochaPrice * 2);
+console.log(lattePrice * 3);
